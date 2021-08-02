d9e9ef2d6e0a590c59352b21b5aaa8df79a0d0f1
Veronika <veronika.pilipenko@gmail.com>
2021-08-02 11:34:04 -0700

    deleted index.html

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do
     resource :user, only: [:create]
     resource :session, only: [:create, :destroy, :show]
-end
+  end
 
-root "static_pages#root"
+  root to: "static_pages#root"
 end
