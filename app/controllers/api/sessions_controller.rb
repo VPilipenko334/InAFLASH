2a36bfa6b2680b7ed0737b893b39e7394661c2c6
Veronika <veronika.pilipenko@gmail.com>
2021-08-02 13:21:39 -0700

    created sessions controller

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,5 +1,22 @@
 class Api::SessionsController < ApplicationController
 
+    def new
+        render :new
+    end
 
-    
+    def create
+        @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
+        if @user.nil?
+            render json: ['Username or Password does not exist'], status: 401 
+        else
+            login!(@user)
+            render 'api/users/show'
+        end
+    end 
+
+    def destroy
+        logout!
+        render json: { message: 'Logout was successful.'}
+    end 
+
 end
