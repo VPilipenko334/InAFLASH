df11b870fd83e9ae605cecbf7307dd0bd8eeecc6
Veronika <veronika.pilipenko@gmail.com>
2021-08-03 16:57:03 -0700

    still buggy but wrote out a container

---
+++ frontend/components/link_bar/link_bar_container.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { connect } from "react-redux";
+import LinkBar from './link_bar_container'
+import { logout } from "../../actions/session_actions";
+
+const mapStateToProps = (state) => ({
+    currentUser: state.session.currentUser,
+});
+
+const mapDispatchToProps = (dispatch) => ({
+    logout: () => dispatch(logout()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(LinkBar);
--- frontend/components/welcome.jsx
+++ frontend/components/welcome.jsx
@@ -2,3 +2,11 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 
+const WelcomePage = () => {
+    return (
+        <div className="body">
+
+        
+        </div>
+    )
+}
