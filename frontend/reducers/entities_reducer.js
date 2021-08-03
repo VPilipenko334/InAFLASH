89179e9267f6ba87f930616940de7b562d6a303f
Veronika <veronika.pilipenko@gmail.com>
2021-08-03 12:38:01 -0700

    Added entitiesReducer

---
+++ frontend/reducers/entities_reducer.js
@@ -0,0 +1,11 @@
+//keeps track of all the relational data in the app 
+//this will combine the reducers that handle the relational data
+
+import { combineReducers } from "redux";
+import usersReducer from './users_reducer';
+
+const entitiesReducer = combineReducers({
+    users: usersReducer
+});
+
+export default entitiesReducer; 
