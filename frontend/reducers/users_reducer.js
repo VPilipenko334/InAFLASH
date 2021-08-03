c029d34e58fdcfdf1db92677a7f159ea39fa7a0d
Veronika <veronika.pilipenko@gmail.com>
2021-08-03 12:32:42 -0700

    added usersReducer

---
+++ frontend/reducers/users_reducer.js
@@ -0,0 +1,16 @@
+// keeps track of all our users 
+
+import { RECEIVE_CURRENT_USER } from "../actions/session_actions";
+
+const userReducer = (oldState = {}, action) => {
+    Object.freeze(oldState);
+    // newState = Object.assign({}, state, { [action.user.id]: action.user })
+    switch (action.type) {
+        case RECEIVE_CURRENT_USER:
+            return Object.assign({}, state, { [action.currentUser.id]: action.currentUser });
+        default:
+            return oldState;
+    }
+};
+
+export default usersReducer; 
