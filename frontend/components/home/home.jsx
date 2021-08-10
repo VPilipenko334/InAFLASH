799619d40d2a612d3b35880ae39e86188ea6072f
Veronika <veronika.pilipenko@gmail.com>
2021-08-09 20:51:37 -0400

    added homepage component

---
+++ frontend/components/home/home.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import PictureFeed from './picturefeed';
+
+class HomePage extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+
+    componentWillMount() {
+        this.props.fetchPictures()
+        //likes here too
+    }
+
+    render() {
+        const { pictures, session } = this.props
+        if (!pictures) {
+            return null 
+        } else {
+            return (
+                <div className="homepage">
+                    <h2>Welcome to the Homepage!</h2>
+                    <ul className="pictures-feed">
+                        {pictures.map((picture, index) => (
+                            <PictureFeed 
+                            key={index}
+                            picture={picture}
+                            session={session}
+                            //likes
+                            //createlike
+                            //deletelike 
+                            />
+                        ))}
+                    </ul>
+                </div>
+            )
+        }
+    }
+}
+
+export default HomePage; 
---
+++ frontend/components/home/home_container.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchPictures } from '../../util/picture_api_util';
+// import likes when they are complete 
+import HomePage from './home'
+
+const mSTP = (state) => {
+    return {
+        pictures: Object.values(state.entities.pictures),
+        //likes 
+        session: state.session,
+    };
+};
+
+const mDTP = (dispatch) => {
+    return {
+        fetchPictures: () => dispatch(fetchPictures()),
+        //fetchLikes
+        //createLikes
+        //deleteLikes
+    }
+}
+
+export default connect(mSTP, mDTP)(HomePage)
