0759d596eaa3315b104cd79e55300df8ca0ad3fc
Veronika <veronika.pilipenko@gmail.com>
2021-09-06 16:30:15 -0400

    added some belongs to and has many to pi pictures

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,22 +1,26 @@
 class Picture < ApplicationRecord
 
-    validates :title, :description, :user_id, presence: true
+    validates :title, :private, :description, :user_id, presence: true
     validates :title, uniqueness: {scope: :user_id}
-    # validate :ensure_post
+    validate :ensure_post
     has_one_attached :picture
-
+    belongs_to :user 
 
     belongs_to :user,
         foreign_key: :user_id,
         class_name: :User
 
-    # has_many :likes, 
-    #     foreign_key: :picture_id,
-    #     class_name: :Like
+    has_many :comments,
+        foreign_key: :picture_id,
+        class_name: :Picture 
 
-    # def ensure_post
-    #     unless self.post.attached?
-    #         errors[:post] << "Must be attached"
-    #     end
-    # end
+    has_many :likes, 
+        foreign_key: :picture_id,
+        class_name: :Like
+
+    def ensure_post
+        unless self.post.attached?
+            errors[:post] << "Must be attached"
+        end
+    end
 end
