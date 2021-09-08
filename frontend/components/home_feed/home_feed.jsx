753debbfc9898722ce668cd5553cc23461787896
Veronika <veronika.pilipenko@gmail.com>
2021-09-07 20:42:47 -0400

    added a home feed page

---
+++ frontend/components/home_feed/home_feed.jsx
@@ -0,0 +1,17 @@
+import React from "react"; 
+import { Link } from "react-router-dom";
+
+class HomeFeed extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hi I am going to be the Home Feed</h1>
+                <p> hi I am the inner home feed </p>
+                    <br/>
+                <Link to="/" className="back-home-link">Click to go back Home</Link>
+            </div>
+        )
+    }
+}
+
+export default HomeFeed; 
