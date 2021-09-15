a571236fb7c4bb0507d6e16ae011aacfdcf6dc43
Veronika <veronika.pilipenko@gmail.com>
2021-09-15 17:28:50 -0400

    home feed

--- frontend/components/home_feed/home_feed.jsx
+++ frontend/components/home_feed/home_feed.jsx
@@ -2,11 +2,14 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 class HomeFeed extends React.Component {
+   
     render() {
+
         return (
             <div>
                 <h1>Hi I am going to be the Home Feed</h1>
                 <p> hi I am the inner home feed </p>
+                <div></div>
                     <br/>
                 <Link to="/" className="back-home-link">Click to go back to the main page</Link>
             </div>
