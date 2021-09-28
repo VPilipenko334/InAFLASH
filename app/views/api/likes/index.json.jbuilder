943603795b12faba093dd2c2ea996105145214e2
Veronika <veronika.pilipenko@gmail.com>
2021-09-28 11:28:33 -0400

    working on editors choice

--- app/views/api/likes/index.json.jbuilder
+++ app/views/api/likes/index.json.jbuilder
@@ -1,5 +1,5 @@
 @likes.each do |like|
     json.set! like.id do
-        json.extract! like, :id, :picture_id, :liker_id
+        json.extract! like, :id, :picture_id, :liker_id, :user_id
     end
 end
---
+++ frontend/components/home_feed/editors_item_pictures.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const EditorItemPage = (props) => {
+    return (
+        <li>
+            <Link to={`/pictures/${picture.id}`} className="picture-render-link">
+                <img className="picture-render" height="300" width="300" src={props.pictures.picture} />
+            </Link>
+        </li>
+    )
+}
+
+export default EditorItemPage; 
+
+
+
+
