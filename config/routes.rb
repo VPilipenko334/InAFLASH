040b612489001f3e95ed843d7ee03e498f45691b
Veronika Pilipenko <veronika.pilipenko@gmail.com>
2023-06-27 23:14:49 -0400

    test

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
- 
+
   namespace :api, defaults: { format: :json } do
-    resources :users, only: [:create, :show, :index, :update] do  
+    resources :users, only: [:create, :show, :index, :update] do
       resources :pictures, only: [:index]
     end
     resource :session, only: [:create, :destroy]
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
     resources :pictures, only: [:show, :index, :create, :destroy] do
       resources :comments, only: [:index]
     end
-    resources :comments, only: [:create, :destroy, :show] 
+    resources :comments, only: [:create, :destroy, :show]
   end
     root to: 'static_pages#root'
 end
